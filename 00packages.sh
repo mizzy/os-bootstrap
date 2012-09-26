da02eb76da1475cc4b80e9ea6813c615dcb0f142
Gosuke Miyashita <gosukenator@gmail.com>
2012-09-26 11:17:52 +0900

    Add packages

--- 00packages.sh
+++ 00packages.sh
@@ -2,10 +2,10 @@
 
 sudo rpm -Uvh http://apt.sw.be/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
-sudo yum install -y gcc make 
+sudo yum install -y gcc gcc-c++ make 
 sudo yum install -y zlib-devel openssl-devel
 sudo yum install -y keychain
-sudo yum install -y db4-devel libxml2-devel expat-devel
+sudo yum install -y db4-devel libxml2-devel expat-devel libxslt-devel
 
 exec $SHELL
 
