7bdd03930b4996bd17ea8167c17a14cda0fdf732
Gosuke Miyashita <gosukenator@gmail.com>
2012-10-18 12:47:16 +0900

    Add readline-devel

--- 00packages.sh
+++ 00packages.sh
@@ -3,7 +3,7 @@
 sudo rpm -Uvh http://apt.sw.be/redhat/el6/en/x86_64/rpmforge/RPMS/rpmforge-release-0.5.2-2.el6.rf.x86_64.rpm
 
 sudo yum install -y gcc gcc-c++ make 
-sudo yum install -y zlib-devel openssl-devel
+sudo yum install -y zlib-devel openssl-devel readline-devel
 sudo yum install -y keychain
 sudo yum install -y db4-devel libxml2-devel expat-devel libxslt-devel
 
